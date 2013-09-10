db10c179c847a755ed75fe5ba6769ef5d5df8b2c
arthur <arthur@playtux.info9.net>
2013-09-09 21:52:44 -0500

    After review, fixes from assignment 1

--- README.md
+++ README.md
@@ -4,3 +4,11 @@ linked-list
 A linked list experiment.
 
 This is the home for my C assignments.
+
+## Building
+
+Just type ```make```
+
+## Running
+
+```$ ./ll -h```
--- ll.c
+++ ll.c
@@ -1,3 +1,9 @@
+/* ll.c
+   Copyright (c) 2013 Arthur Marble
+   This project is licensed under the MIT license.
+   See LICENSE for complete description.
+*/
+
 #include<stdio.h>
 #include<stdlib.h>
 #include<unistd.h>
@@ -7,18 +13,20 @@
 #define VERSION_NUMBER 0.1
 #define VERSION_STRING_HELPER(X) #X
 #define VERSION_STRING(X) VERSION_STRING_HELPER(X)
+#define TRUE 1
+#define FALSE 0
 
 struct myArgs_t
 {
-        int verbose; // The -v --verbose args to turn on verbose mode
-        int help; // The -h --help args to print a help message
-        int version; // The -V --version args to print the version
-        int print; // The -p --print args to print the linked lists !JUST PRINTS OUT "Hello, World!" for now
-        int debug; // WILL MAKE VERBOSE TAKE DEBUG PLACE
+        int verbose;     // The -v --verbose args to turn on verbose mode
+        int help;        // The -h --help args to print a help message
+        int version;     // The -V --version args to print the version
+        int print;       // The -p --print args to print the linked lists
+        int debug;       // WILL MAKE VERBOSE TAKE DEBUG PLACE
         char *debug_opt; // The --debug option which will print options given
 } myArgs;
 
-static const char *optString = "vhVpd:";
+static const char *optString = "vhVpd;";
 static const struct option longOpts[] = {
         { "verbose", no_argument, NULL, 'v'
         },
@@ -28,74 +36,88 @@ static const struct option longOpts[] = {
         },
         { "print", no_argument, NULL, 'p'
         },
-        { "debug", required_argument, NULL, 'd'
+        { "debug", optional_argument, NULL, 'd'
         },
         { NULL, no_argument, NULL, 0
         }
 };
+
+void usage(int exit_code)
+{
+        printf("Usage: ll [OPTION]\n\nMandatory arguments for long options are"
+               " also mandatory for short options\n  -v, --verbose\t\trun program verbosely\n"
+               "  -h, --help\t\tprint this help message\n  -V, --version\t\tprint the program version\n"
+               "  -p, --print\t\tprint the linked lists\n  -d, --debug=ANYTHING\tprint the state of myArgs and option"
+               " used for -d, --debug=ANYTHING\n\n");
+        exit(exit_code);
+}
+
+void print_list()
+{
+        printf("Hello, World 2!\n");
+}
 
 int main(int argc, char *argv[])
 {
         int opt = 0;
 
         // Initialize global arguments
-        myArgs.verbose = 0; // 0 == false
-        myArgs.help = 0;
-        myArgs.version = 0;
-        myArgs.print = 0;
-        myArgs.debug = 0;
+        myArgs.verbose = FALSE;
+        myArgs.help = FALSE;
+        myArgs.version = FALSE;
+        myArgs.print = FALSE;
+        myArgs.debug = FALSE;
         myArgs.debug_opt = NULL;
-        int *longIndex = NULL;
+        int *longIndex = NULL; // cheap hack
 
 
         opt = getopt_long(argc, argv, optString, longOpts, longIndex);
-        while(opt != -1) {
-                switch(opt) {
+        while (opt != -1) {
+                switch (opt) {
                 case 'v':
-                        myArgs.verbose = 1; // 1 == true
+                        myArgs.verbose = TRUE;
                         break;
                 case 'h':
-                        myArgs.help = 1;
+                        myArgs.help = TRUE;
                         break;
                 case 'V':
-                        myArgs.version = 1;
+                        myArgs.version = TRUE;
                         break;
                 case 'p':
-                        myArgs.print = 1;
+                        myArgs.print = TRUE;
                         break;
                 case 'd':
-                        myArgs.debug = 1;
+                        myArgs.debug = TRUE;
                         myArgs.debug_opt = optarg;
                         break;
-                case 0: // long option without a short arg (NO LONG OPTION WITHOUT A SHORT ARG SO BREAK)
-                        break;
+//                case 0: // long option without a short arg (NO LONG OPTION WITHOUT A SHORT ARG SO BREAK)
+//                        usage(1);
+//                        break;
                 default:
                         /* you should not get to here unless you have a invalid argument */
+                        usage(1);
                         break;
                 }
                 opt = getopt_long(argc, argv, optString, longOpts, longIndex);
         }
-        if(myArgs.verbose == 1) {
+        if (myArgs.help == TRUE) {
+                usage(0);
+        }
+        if (myArgs.verbose == TRUE) {
                 printf("Running program verbosely...\n");
         }
-        if(myArgs.help == 1) {
-                printf("Usage: ll [OPTION]\n\nMandatory arguments for long options are"
-                       " also mandatory for short options\n  -v, --verbose\t\trun program verbosely\n"
-                       "  -h, --help\t\tprint this help message\n  -V, --version\t\tprint the program version\n"
-                       "  -p, --print\t\tprint the linked lists\n  -d, --debug=ANYTHING\tprint the state of myArgs and option"
-                       " used for -d, --debug=ANYTHING\n\n");
-        }
-        if(myArgs.version == 1) {
+        if (myArgs.version == TRUE) {
                 char *p = VERSION_STRING(VERSION_NUMBER);
                 printf("Linked Lists version: %s\n", p);
+                exit(0);
         }
-        if(myArgs.print == 1) {
-                printf("Hello, World!\n");
-        }
-        if(myArgs.debug == 1) {
+        if (myArgs.debug == TRUE) {
                 printf("the -d options: %s\n", myArgs.debug_opt);
                 printf("-v: %d, -h: %d, -V: %d, -p: %d, -d: %d\n", myArgs.verbose, myArgs.help,
                        myArgs.version, myArgs.print, myArgs.debug);
+        }
+        if (myArgs.print == TRUE) {
+                print_list();
         }
 
         return 0;
